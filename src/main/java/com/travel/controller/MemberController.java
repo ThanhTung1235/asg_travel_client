e8695bea346753f779dc9586a83461dc0557ac29
thjennhjenoxi <sondtd00591@fpt.edu.vn>
2019-10-15 18:28:49 +0700

    create login

--- src/main/java/com/travel/controller/MemberController.java
+++ src/main/java/com/travel/controller/MemberController.java
@@ -1,5 +1,6 @@
 package com.travel.controller;
 
+import com.google.gson.Gson;
 import member.Member;
 import member.MemberService;
 import member.MemberServiceImplServiceLocator;
@@ -9,12 +10,16 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpSession;
+import java.rmi.RemoteException;
+
 @RequestMapping(value = "/member")
 @Controller
 public class MemberController {
 
     private static final String MEMBERFORM = "member/create";
     private static final String MEMBERLIST = "member/list";
+    private static final String MEMBERLOGIN = "member/login";
 
     @Autowired
     MemberService memberService;
@@ -46,5 +51,28 @@ public class MemberController {
             ex.printStackTrace();
         }
         return "redirect:/member";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String showFormLogin(Model model){
+        model.addAttribute("member", new Member());
+        return MEMBERLOGIN;
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String Login(Member member, HttpSession httpSession){
+        try {
+            Member m = memberService.authentication(member.getName(), member.getPassword());
+            if ( m != null){
+                System.out.println(new Gson().toJson(m));
+                httpSession.setAttribute("member", m);
+                return "redirect:/place";
+            }else {
+                return "redirect:/member/login";
+            }
+        } catch (RemoteException e) {
+            e.printStackTrace();
+            return "redirect:/member/login";
+        }
     }
 }
